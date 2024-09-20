e7481777b47ad9183f9744431abab9621d9b2308
Nazar Bartosik <nazar.bartosik@cern.ch>
2024-09-20 17:30:57 +0200

    Fixed indentation

--- generation/bib/data_formats.py
+++ generation/bib/data_formats.py
@@ -4,8 +4,8 @@ import numpy as np
 
 # Format definitions of the FLUKA-generated Beam Induced Background (BIB) samples
 DATA_FORMATS = {
-    # Kinematics of the BIB particle + mother particle + position of the muon decay
-    '1': np.dtype([
+	# Kinematics of the BIB particle + mother particle + position of the muon decay
+	'1': np.dtype([
 		('fid',    np.int32),
 		('fid_mo', np.int32),
 		('e_kin',  np.float64),
@@ -27,9 +27,9 @@ DATA_FORMATS = {
 		('pz_mo',  np.float64),
 		('age_mo', np.float64)
 		]),
-    # No info about the mother particle
-    '2': np.dtype([
-        ('fid',    np.int32),
+	# No info about the mother particle
+	'2': np.dtype([
+		('fid',    np.int32),
 		('fid_mo', np.int32),
 		('e_kin',  np.float64),
 		('x',      np.float64),
@@ -43,9 +43,9 @@ DATA_FORMATS = {
 		('y_mu',   np.float64),
 		('z_mu',   np.float64)
 	]),
-    # Added back kinematics of the mother particle, w/o age
-    '3': np.dtype([
-        ('fid',    np.int32),
+	# Added back kinematics of the mother particle, w/o age
+	'3': np.dtype([
+		('fid',    np.int32),
 		('fid_mo', np.int32),
 		('e_kin',  np.float64),
 		('x',      np.float64),
@@ -67,7 +67,7 @@ DATA_FORMATS = {
 	]),
 
 	# Format definitions of the Incoherent Pair Production (IPP) samples generated with Guinea Pig
-    'ip1': np.dtype([
+	'ip1': np.dtype([
 		('fid',    np.int32),
 		('fid_mo', np.int32),
 		('e_kin',  np.float64),
